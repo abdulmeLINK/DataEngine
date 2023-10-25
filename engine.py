e159b43f0aa1e6dc630e80da932bd0413304e4c1
Abdulmelik Saylan <47142956+abdulmeLINK@users.noreply.github.com>
2023-10-25 04:19:01 +0300

    transforming to argparse

--- engine.py
+++ engine.py
@@ -1,86 +1,98 @@
 import uniModel as model
 
 import json
-
 import sys
-import getopt
+import argparse
 
 
 class engine:
-    def __init__(self):
-        self.argv = sys.argv
+    def __init__(self, defaultMainKey='mainKey'):
+        self.parser = parser = argparse.ArgumentParser(
+            description='Engine for handling data.')
+        parser.add_argument('commands', metavar='C', type=str,
+                            nargs='*', help='commands to execute')
+        parser.add_argument('--main-key', dest='mainKey', action='store',
+                            default=defaultMainKey, help='sets the main-key to be read from JSON')
+        parser.add_argument('-q', '--query', dest='query',
+                            action='store', help='passes a query string', default='')
+        parser.add_argument('-s', '--sort-by', dest='sortBy',
+                            action='store', help='sorts results by a key', default='')
+        parser.add_argument('-m', '--modify', dest='modify',
+                            action='store_true', help='modification/modifier')
+        parser.add_argument('-d', '--delete', dest='delete',
+                            action='store_true', help='record remove operation modifier')
+        parser.add_argument('-k', '--keys', dest='keys',
+                            action='store_true', help='shows accessible keys')
+        parser.add_argument('-a', '--all', dest='all',
+                            action='store_true', help='gets saved data')
+        parser.add_argument('--save', dest='save',
+                            action='store_true', help='saves data')
+
+        args = parser.parse_args()
+        self.mainKey = args.mainKey
+        self.argquery = args.query
+        self.argsortBy = args.sortBy
+        self.argmodify = args.modify
+        self.argdelete = args.delete
+        self.argkeys = args.keys
+        self.argall = args.all
+        self.argsave = args.save
+        self.argcommands = args.commands
+
         self.read()
         self.writeable = json.load(self.fileObj)
         has = False
         for key in self.writeable.keys():
-            if key == 'unis':
+            if key == self.mainKey:
                 has = True
         if not has:
-            self.writeable['unis'] = []
+            self.writeable[self.mainKey] = []
 
-    def main(self):
-        query = ''
-        sortByArg = ''
-        helper = 'frame.py' + '\nall: gets saved data' + '\nsave: saves data' + '\n-q, --query <query_string>: passes a query string' + '\n-m, --modify: modification modifier' + \
-            '\n-m, --modify: record modification operation modifier' + '\n-d, --delete: record remove operation modifier' + \
-            '\n-k, --keys, keys: shows accessible keys' + \
-            '\n-s, --sort-by <key>: sorts results by a key'
-        self.check()
-        try:
-            opts, args = getopt.getopt(
-                self.argv[1:], "hmq:dps:ak", ["help", "query=", "modify", "delete", "point", "sort-by=", "all", "keys"])
-        except getopt.GetoptError:
-            print(helper)
-            sys.exit(2)
-        for opt, arg in opts:
-            if opt in ("-h", "--help"):
-                print(helper)
-                sys.exit()
-            if opt in ("-q", "--query"):
-                query = arg
-            if opt in ("-s", "--sort-by"):
-                sortByArg = arg.lower().replace(" ", "")
-            if opt in ("-m", "--modify"):
-                self.modify(query, sortByArg)
-            if opt in ("-d", "--delete"):
-                self.delete(query, sortByArg)
-            if opt in ("-p", "--point"):
-                self.modifyPoints(query, sortByArg)
-            if opt in ("-a", "--all"):
-                self.all()
-            if opt in ("-k", "--keys"):
-                print(model.uniModel().onlyLetterDict().keys())
-        if query != '':
-            self.search(query, sortByArg)
+    def run(self):
+        if 'help' in self.argcommands:
+            print(self.parser.format_help())
+            sys.exit()
 
-        if 'save' in self.argv[1:]:
+        if self.argquery:
+            self.search(self.argquery, self.argsortBy)
+
+        if self.argmodify:
+            self.modify(self.argquery, self.argsortBy)
+
+        if self.argdelete:
+            self.delete(self.argquery, self.argsortBy)
+
+        if self.argall or 'all' in self.argcommands:
+            self.all(self.argsortBy)
+
+        if self.argkeys or 'keys' in self.argcommands:
+            print(model.uniModel().onlyLetterDict().keys())
+
+        if self.argsave or 'save' in self.argcommands:
             self.save()
 
-        if 'all' in self.argv[1:]:
-            self.all(sortByArg)
-
-        if 'keys' in self.argv[1:]:
-            self.all(sortByArg)
-
     def check(self):
-        for element in self.writeable['unis']:
+        for element in self.writeable[self.mainKey]:
             uni = model.uniModel()
             uni.fromJson(element)
-            index = self.writeable['unis'].index(element)
+            index = self.writeable[self.mainKey].index(element)
             if uni.searchMissing(element):
-                self.writeable['unis'][index] = uni.toJson()
+                self.writeable[self.mainKey][index] = uni.toJson()
                 self.overwrite()
 
     def all(self, order):
         res = []
-        for element in self.writeable['unis']:
+        for element in self.writeable[self.mainKey]:
             uni = model.uniModel()
             uni.fromJson(element)
             res.append(uni)
         print(len(res), 'results found')
+
         return self.sortBy(res, order)
 
     def sortBy(self, res, order):
+        if not (len(res) > 0):
+            return
         onlyLetter = res[0].onlyLetterDict()
         if order in onlyLetter.keys():
             res.sort(key=lambda x: x.__dict__[onlyLetter[order]])
@@ -89,14 +101,18 @@ class engine:
             uni.show()
 
     def search(self, q, order):
+
         res = []
-        for element in self.writeable['unis']:
+        for element in self.writeable[self.mainKey]:
             uni = model.uniModel()
             uni.fromJson(element)
             if q in uni.name:
                 res.append(uni)
         print(len(res), 'results found')
-        return self.sortBy(res, order)
+        if self.sortBy is not None:
+            return self.sortBy(res, order)
+        else:
+            return res
 
     def modify(self, q, order):
         if q != '' and q != ' ':
@@ -108,10 +124,10 @@ class engine:
             exit()
         index = int(
             input('Please enter index of result that modification needed: '))
-        old = self.writeable['unis'].index(results[index].toJson())
+        old = self.writeable[self.mainKey].index(results[index].toJson())
         new = results[old].inputFromUser()
         print('='*10)
-        self.writeable['unis'][old] = new.toJson()
+        self.writeable[self.mainKey][old] = new.toJson()
         self.overwrite()
         print('Modified to:')
         new.show()
@@ -130,7 +146,7 @@ class engine:
         selected = results[index]
         named = selected.name
         print(named)
-        self.writeable['unis'].remove(selected.toJson())
+        self.writeable[self.mainKey].remove(selected.toJson())
         print('=' * 20)
         self.overwrite()
         print(f'{named} named and {str(index)} indexed result successfully deleted')
@@ -146,7 +162,7 @@ class engine:
         index = int(
             input('Please enter index of result that point addition operation needed: '))
         selected = results[index]
-        index = self.writeable['unis'].index(selected.toJson())
+        index = self.writeable[self.mainKey].index(selected.toJson())
         selected.show()
         while True:
             points = input(
@@ -154,11 +170,11 @@ class engine:
             selected.points += int(points)
             print('=' * 10)
             print('new point value:', selected.points)
-            self.writeable['unis'][index] = selected.toJson()
+            self.writeable[self.mainKey][index] = selected.toJson()
             self.overwrite()
 
     def append(self, mem):
-        self.writeable['unis'] = self.writeable['unis'] + mem
+        self.writeable[self.mainKey] = self.writeable[self.mainKey] + mem
         f = open(self.fileName, 'w')
         f.write(json.dumps(self.writeable))
 
@@ -191,5 +207,4 @@ class engine:
 
 
 if __name__ == '__main__':
-    run = engine()
-    run.main()
+    engine().run()
